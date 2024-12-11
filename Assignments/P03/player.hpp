bed6e75927def4cb932a490b9d6e86d191b09cc6
rjae5 <rykirjoe@yahoo.com>
2024-12-11 00:33:49 -0600

    Generalize attributes

--- Assignments/P03/player.hpp
+++ Assignments/P03/player.hpp
@@ -7,30 +7,32 @@ class Player
 {
     std::string uName;
     sf::Font font;
+    sf::Text uNameText;
     
 public:
     Player(std::string un)
     {
+        // Save username
         uName = un;
-        // Load font
+        
+         // Load font
         if (!font.loadFromFile("./media/fonts/arial.ttf")) 
         {
             // Handle error
-            std::cout << "Error loading font!" << std::endl;
+            std::cerr << "Error loading font!" << std::endl;
         }
 
-        else 
-            std::cout << "Successfully loaded font" << std::endl;
+        // Convert username to sf::Text
+        uNameText.setString(uName);
+        uNameText.setFont(font);
+        uNameText.setCharacterSize(24);
+        uNameText.setPosition(0,0);
+        uNameText.setFillColor(sf::Color::Blue);
     }
 
     sf::Text getName()
     {
-        static sf::Text uNameText(uName, font, 24);
-        uNameText.setPosition(0,0);
-        uNameText.setFillColor(sf::Color::Blue);
-
+        //return name in sf::Text format
         return uNameText;
     }
-
-
 };
